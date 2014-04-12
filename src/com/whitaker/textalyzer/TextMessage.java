0610c52c9da8e089c5f3cdd53ed4044568580182
Roy Iacob <iacobroy@gmail.com>
2014-04-12 04:00:53 -0700

    timeCreated from Date -> long

--- src/com/whitaker/textalyzer/TextMessage.java
+++ src/com/whitaker/textalyzer/TextMessage.java
@@ -1,19 +1,17 @@
 package com.whitaker.textalyzer;
 
-import java.util.Date;
-
 public class TextMessage 
 {
 	private enum Directions {OUTBOUND, INBOUND};
 	private Directions direction;
 	private String body;
-	private Date timeCreated;
+	private long timeCreated;
 
 	public TextMessage(Directions direction, String body, int timeCreated) 
 	{
 		this.direction = direction;
 		this.body = body;
-		this.timeCreated = new Date(timeCreated); //Might need to multiple by 1000L
+		this.timeCreated = timeCreated; //Might need to multiple by 1000L
 	}
 }
 
