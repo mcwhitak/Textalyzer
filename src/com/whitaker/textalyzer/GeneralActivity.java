dc6e95d44f1bef57a249ab28bda397e479755b22
Michael Whitaker <mcwhitak@usc.edu>
2014-05-25 12:53:04 -0700

    Hopefully working headers in General stats and temporary fix for general stats wasteful logic

--- src/com/whitaker/textalyzer/GeneralActivity.java
+++ src/com/whitaker/textalyzer/GeneralActivity.java
@@ -1,9 +1,12 @@
 package com.whitaker.textalyzer;
 
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
+
 import com.whitaker.textalyzer.util.BounceListView;
 import com.whitaker.textalyzer.util.TextalyzerActivity;
+
 import android.app.Activity;
 import android.content.res.Resources;
 import android.graphics.Color;
@@ -20,16 +23,6 @@ import android.widget.TextView;
 public class GeneralActivity extends TextalyzerActivity
 {
 	private BounceListView itemListView;
-	private int [] textOGram = new int[]{0,0,0,0,0,0,
-										0,0,0,0,0,0,
-										0,0,0,0,0,0,
-										0,0,0,0,0,0};
-	
-	private String[] categories = {"Total Incoming Texts", "Total Outgoing Texts","Most Likely to Start Conversations...", "With you", "You started",
-			"Least Likely to Start Conversations...","With", "With you","Frequency of texts by hour",
-			"12 am","1 am","2 am","3 am","4 am","5 am","6 am","7 am","8 am","9 am","10 am","11 am",
-			"12 pm","1 pm","2 pm","3 pm","4 pm","5 pm","6 pm","7 pm","8 pm","9 pm","10 pm","11 pm"
-	};
 	
 	private static HashMap<String, ContactHolder> contactMap = MainActivity.contactMap;
 	
@@ -45,14 +38,33 @@ public class GeneralActivity extends TextalyzerActivity
 	private String leastFavoriteContactForYou = "None"; // by started conversation - your started conversations
 	private String leastFavoriteContactForThem = "None"; // by started conversation - your started conversations
 	
+	private ArrayList<String> adapterInstructions;
 	
+	private int [] textOGram = new int[]{0,0,0,0,0,0,
+			0,0,0,0,0,0,
+			0,0,0,0,0,0,
+			0,0,0,0,0,0};
+
+	private String[] categories = {"Total Incoming Texts", "Total Outgoing Texts","Most Likely to Start Conversations...", "With you", "You started",
+					"Least Likely to Start Conversations...","With", "With you","Frequency of texts by hour",
+					"12 am","1 am","2 am","3 am","4 am","5 am","6 am","7 am","8 am","9 am","10 am","11 am",
+					"12 pm","1 pm","2 pm","3 pm","4 pm","5 pm","6 pm","7 pm","8 pm","9 pm","10 pm","11 pm"};
+	
+	private int[] type = {0, 0, 1, 0, 0, 1 ,0, 0, 1, 
+					0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+					0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0};
+
+	
+	
+	@SuppressWarnings("deprecation")
 	@Override
 	protected void onCreate(Bundle savedInstanceState)
 	{
 		super.onCreate(savedInstanceState);
 		this.setContentView(R.layout.general_stats);		
 		grabAllViews();
-		itemListView.setAdapter(new ItemAdapter());
+		
+		adapterInstructions = new ArrayList<String>();
 		
 		int leastFavoriteContactForYouCount = 0;
 		int leastFavoriteContactForThemCount = 0;
@@ -89,11 +101,26 @@ public class GeneralActivity extends TextalyzerActivity
 			
 			for (TextMessage t: c.textMessages)
 			{
-				textOGram[new Date(t.timeCreated).getHours()]++;
-				//Log.d("Royyy",new Date(t.timeCreated).getHours() + ": " + textOGram[new Date(t.timeCreated).getHours()]);
+				textOGram[new Date(t.timeCreated).getHours()]++; //getHours Deprecated & Overridden
 			}	
 		}
-
+		
+		//Create text for adapter
+		adapterInstructions.add(Integer.toString(totalIncomingTexts));
+		adapterInstructions.add(Integer.toString(totalOutgoingTexts));
+		adapterInstructions.add("");
+		adapterInstructions.add(mostLikeleyToStartWithYou + " by " + mostLikeleyToStartWithYouCount);
+		adapterInstructions.add(mostLikeleyToStartWithThem + " by " + mostLikeleyToStartWithThemCount);
+		adapterInstructions.add("");
+		adapterInstructions.add(leastFavoriteContactForYou);
+		adapterInstructions.add(leastFavoriteContactForThem);
+		adapterInstructions.add("");
+		for(int i=9; i<24; i++)
+		{
+			adapterInstructions.add(Integer.toString(textOGram[i-7]) + " texts, " + (double)Math.round((100*(double)textOGram[i-7]/(totalIncomingTexts+totalOutgoingTexts)) * 10) / 10 + "%");
+		}
+		
+		itemListView.setAdapter(new ItemAdapter());
 		 //by started conversations
 
 	}
@@ -126,64 +153,43 @@ public class GeneralActivity extends TextalyzerActivity
 		@Override
 		public View getView(int position, View convertView, ViewGroup parent)
 		{
+			int resID;
+			if(type[position] == 0)
+			{
+				resID = R.layout.general_item;
+			}
+			else
+			{
+				resID = R.layout.general_header;
+			}
+			
 			View itemView = convertView;
-			if(convertView == null)
+			if(convertView == null || convertView.getId()!=resID)
 			{
 				LayoutInflater li = getCtx().getLayoutInflater();
-				itemView = li.inflate(R.layout.general_item, null);
+				itemView = li.inflate(resID, null);
 			}
 			
-			TextView catText = (TextView)itemView.findViewById(R.id.general_item_name);
-			TextView resultText = (TextView)itemView.findViewById(R.id.general_item_result);
-			
-			if(position < categories.length)
+			if(resID == R.layout.general_item)
 			{
-				catText.setText(categories[position]);
-				if (position == 0)
+				TextView catText = (TextView)itemView.findViewById(R.id.general_item_name);
+				TextView resultText = (TextView)itemView.findViewById(R.id.general_item_result);
+			
+				if(position < categories.length)
 				{
-					resultText.setText(Integer.toString(totalIncomingTexts)); 
+					catText.setText(categories[position]);
+					resultText.setText(adapterInstructions.get(position));
 				}
-				else if (position == 1)
-				{
-					resultText.setText(Integer.toString(totalOutgoingTexts));
-				}
-				else if (position == 2)
-				{
-					resultText.setText("");
-				}
-				
-				else if (position == 3)
-				{
-					resultText.setText(mostLikeleyToStartWithYou + " by " + mostLikeleyToStartWithYouCount);
-				}
-				else if (position == 4)
-				{
-					resultText.setText(mostLikeleyToStartWithThem + " by " + mostLikeleyToStartWithThemCount);
-				}
-				else if (position == 5)
-				{
-					
-					resultText.setText("");
-				}
-				
-				else if (position == 6)
-				{
-					resultText.setText(leastFavoriteContactForYou);
-				}
-				else if (position == 7)
-				{
-					resultText.setText(leastFavoriteContactForThem);
-				}
-				else if (position == 8)
-				{
-					resultText.setText("");
-				}
-				else if (position > 8 && position < 24)
-				{
-					resultText.setText(Integer.toString(textOGram[position-7]) + " texts, " + (double)Math.round((100*(double)textOGram[position-7]/(totalIncomingTexts+totalOutgoingTexts)) * 10) / 10 + "%");	
-				}
-
 			}
+			else if(resID == R.layout.general_header)
+			{
+				TextView breakText = (TextView)itemView.findViewById(R.id.breaktext);
+				if(position < categories.length)
+				{
+					breakText.setText(categories[position]);
+				}
+			}
+		
 			return itemView;
 		}
 		
